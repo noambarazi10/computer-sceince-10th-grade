9ab75f04df63cd0c765018ac93a2620621f5d414
noambarazi10 <144588063+noambarazi10@users.noreply.github.com>
2023-12-31 10:59:47 +0200

    Update class 4.java

--- class 4.java
+++ class 4.java
@@ -102,3 +102,74 @@ public class Main {
         }
     }
 }
+תרגיל 5:
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         
+        System.out.print("Enter a four letters word");
+        String inputString = scanner.next();
+
+        if (inputString.length() == 4) {
+            String modifiedString = inputString.replaceAll("[uoiea]", "*");
+
+            System.out.println("The new word is: " + modifiedString);
+        } else {
+            System.out.println("The word is not four words long");
+        }
+
+        scanner.close();
+    }
+}
+תרגיל 6:
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int rand = random.nextInt(0,100) + 1;
+        System.out.println(rand);
+        if (rand == 0) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 1) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 2) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 3) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 4) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 5) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 6) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 7) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 8) {
+            System.out.println("This number is single digit number");
+        } 
+        else if (rand == 9) {
+            System.out.println("This number is single digit number");
+        } 
+        else {
+            System.out.println("This number is a two digit number");
+        }
+        if (rand % 2 == 0) {
+            System.out.println("This number is divided by two");
+        }
+        if (rand % 3 == 0) {
+            System.out.println("This number is divided by three");
+        }
+    }
+}
